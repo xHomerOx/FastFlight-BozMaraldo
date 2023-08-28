ae831ac10588e339ead6c2a6e2e7db124ec1e5a1
OTELLO BOZ MARALDO <oboz@agn.gov.ar>
2023-08-28 19:52:06 -0300

    Removed Dynamic JSON

--- flights.js
+++
@@ -1,9 +0,0 @@
-const airports = ["Buenos Aires (EZE)", "La Paz (LPB)", "Brasilia (BSB)", "Santiago (AMB)", "Bogotá (BOG)", "Quito (UIO)", "Georgetown (GEO)", "Asunción (ASU)", "Lima (LIM)", "Zanderij (PBM)", "Montevideo (MVD)", "Caracas (CCS)"];
-
-let flightsData = [];
-for(i = 1; i <= 100; i++) {
-    const sourceAirport = Math.floor(Math.random() * airports.length);
-    const destinationAirport = Math.floor(Math.random() * airports.length);
-    myFlight = { source: airports[sourceAirport], destination: airports[destinationAirport] }
-    flightsData.push(myFlight);
-}
